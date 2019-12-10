e4ed358fd903c99efcd137b5c0c8d324b814d9d5
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-10 08:56:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  your_hash = {"name" => "Kamille", "age" => "31", "favorite_food" = "Sinigang"}
+  your_hash = {"name" => "Kamille", "age" => "31", "favorite_food" => "Sinigang"}
 end
 
 
